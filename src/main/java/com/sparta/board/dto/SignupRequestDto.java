e3e309de1056d6112250e6ca8ca6c9e0e21fa0fa
sanggyu <limsanggyu@kakao.com>
2023-03-19 17:07:46 +0900

    CI/CD 테스트2

--- src/main/java/com/sparta/board/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/board/dto/SignupRequestDto.java
@@ -21,6 +21,8 @@ public class SignupRequestDto {
             message = "8~15글자, 글자 1개, 숫자 1개, 특수문자 1개 꼭 입력해야합니다.")
     private String password;
 
+    private String nickname;
+
     private boolean admin;
     private String adminToken = "";
 
