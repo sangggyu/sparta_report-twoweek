12d4bc3f950188cc91bd62288c753f4ab89347b6
sanggyu <limsanggyu@kakao.com>
2023-03-20 09:02:58 +0900

    CI/CD 테스트9

--- src/main/java/com/sparta/board/controller/HomeController.java
+++ src/main/java/com/sparta/board/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.sparta.board.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class HomeController {
 
+    @CrossOrigin
     @GetMapping("/")
     public ModelAndView home() {
         return new ModelAndView("index");
