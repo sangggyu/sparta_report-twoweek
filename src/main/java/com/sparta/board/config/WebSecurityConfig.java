2cee3fae7fd17d4008712b6ad74941bd4d1f5bb2
sanggyu <limsanggyu@kakao.com>
2023-03-21 16:07:19 +0900

    CI/CD 테스트44

--- src/main/java/com/sparta/board/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/board/config/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 
@@ -26,13 +26,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true)
+
 public class WebSecurityConfig {
 
     private final JwtUtil jwtUtil;
--- src/main/java/com/sparta/board/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/board/dto/SignupRequestDto.java
@@ -14,11 +14,11 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class SignupRequestDto {
     @NotBlank(message = "아이디는 필수 입력값입니다.")
-    @Pattern(regexp = "^*[a-z0-9]{4,10}$", message = "4~10글자 알파벳 소문자(a~z), 숫자(0~9)만 입력 가능합니다.")
+//    @Pattern(regexp = "^*[a-z0-9]{4,10}$", message = "4~10글자 알파벳 소문자(a~z), 숫자(0~9)만 입력 가능합니다.")
     private String username;
     @NotBlank(message = "비밀번호는 필수 입력값 입니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-={}\\[\\]\\|:;\"'<>,.?\\/]).{8,15}$",
-            message = "8~15글자, 글자 1개, 숫자 1개, 특수문자 1개 꼭 입력해야합니다.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-={}\\[\\]\\|:;\"'<>,.?\\/]).{8,15}$",
+//            message = "8~15글자, 글자 1개, 숫자 1개, 특수문자 1개 꼭 입력해야합니다.")
     private String password;
 
 
