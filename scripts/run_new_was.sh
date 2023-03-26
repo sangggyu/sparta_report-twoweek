53f3aaafe51b60321ec5104f809f639a3f98af76
sanggyu <limsanggyu@kakao.com>
2023-03-26 21:56:22 +0900

    nginX 3

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -10,7 +10,7 @@ TARGET_PORT=0
 echo "> Current port of running WAS is ${CURRENT_PORT}."
 
 if [ ${CURRENT_PORT} -eq 8081 ]; then
-  TARGET_PORT=8080
+  TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
   TARGET_PORT=8081
 else
